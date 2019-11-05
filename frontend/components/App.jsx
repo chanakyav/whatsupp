2a55064cc92fcdb2c7f1669c2c27150ef0d9c248
Chanakya <chanakya.valluri@gmail.com>
2019-11-05 13:28:24 -0800

    added routes for login/signup

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import GreetingContainer from './greeting/greeting_container'
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+import LoginFormContainer from "./session_form/login_form_container";
+import SignupFormContainer from "./session_form/signup_form_container";
 
 const App = () => (
     <div>
-        <img src="https://nick-2018.s3.amazonaws.com/logo.png" />
-        <h1>WhatsUpp</h1>
+        <header>
+            <img src="https://nick-2018.s3.amazonaws.com/logo.png" />
+            <h1>WhatsUpp</h1>
+            <GreetingContainer />
+        </header>
+        
+        <Route path="/login" component={LoginFormContainer}/>
+        <Route path="/signup" component={SignupFormContainer}/>
     </div>
 );
 
