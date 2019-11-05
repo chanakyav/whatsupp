85dc65ba9d3f3fe2c4b44bcb2329107806bf1f42
Chanakya <chanakya.valluri@gmail.com>
2019-11-05 10:46:25 -0800

    created App component

---
+++ frontend/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <img src="https://nick-2018.s3.amazonaws.com/logo.png" />
+        <h1>WhatsUpp</h1>
+    </div>
+);
+
+export default App;
