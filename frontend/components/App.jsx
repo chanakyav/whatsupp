ae27b86b95b6d379822ebc411c96d6c709e00633
Chanakya <chanakya.valluri@gmail.com>
2019-11-05 14:06:23 -0800

    added routes. front end user auth done

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,17 +9,21 @@ import {
 } from 'react-router-dom';
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
     <div>
         <header>
-            <img src="https://nick-2018.s3.amazonaws.com/logo.png" />
-            <h1>WhatsUpp</h1>
+            <Link to="/">
+                <img src="https://nick-2018.s3.amazonaws.com/logo.png" />
+                <h1>WhatsUpp</h1>
+            </Link>
             <GreetingContainer />
         </header>
-        
-        <Route path="/login" component={LoginFormContainer}/>
-        <Route path="/signup" component={SignupFormContainer}/>
+        <Switch>
+            <AuthRoute exact path="/login" component={LoginFormContainer}/>
+            <AuthRoute exact path="/signup" component={SignupFormContainer}/>
+        </Switch>
     </div>
 );
 
