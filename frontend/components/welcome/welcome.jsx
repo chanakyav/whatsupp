939e655f18818ecaeeb3745ad131b086890e0fde
Chanakya <chanakya.valluri@gmail.com>
2019-11-06 15:19:19 -0800

    created welcome component

---
+++ frontend/components/welcome/welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+    return (
+        <div className="login-box welcome-container">
+            <p >Welcome to WhatsUpp!</p>
+        </div>
+    )
+}
+
+export default Welcome;
