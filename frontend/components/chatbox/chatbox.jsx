0c6cf544ba5b71580f018209ec12fec02b9e3211
Chanakya <chanakya.valluri@gmail.com>
2019-11-07 10:53:30 -0800

    created chatbox component and scss

---
+++ frontend/components/chatbox/chatbox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ChatBox extends Component {
+    render() {
+        return (
+            <div className="chatbox-container">
+                ChatBox
+            </div>
+        )
+    }
+}
