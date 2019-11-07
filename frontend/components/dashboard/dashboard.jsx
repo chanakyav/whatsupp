476f96c831c47150d77a5607cf71423117149c46
Chanakya <chanakya.valluri@gmail.com>
2019-11-07 11:10:09 -0800

    displaying dashboard with profile and chatboard

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Profile from '../profile/profile'
+import ChatBox from '../chatbox/chatbox'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className="dashboard-container">
-                
+                <Profile/>
+                <ChatBox/>
             </div>
         )
     }
