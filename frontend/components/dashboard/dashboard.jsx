bfdfe9596671bd1fc73358e400ac9ca663fa8a4e
Chanakya <chanakya.valluri@gmail.com>
2019-11-07 12:57:07 -0800

    add contacts component to dashboard

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import Profile from '../profile/profile'
 import ChatBox from '../chatbox/chatbox'
+import Contacts from '../contacts/contacts'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className="dashboard-container">
-                <Profile/>
+                <div className="left-pane">
+                    <Profile/>
+                    <Contacts/>
+                </div>
                 <ChatBox/>
             </div>
         )
