f663058c87765bfb1f0c000276c9a2babf08f531
Chanakya <chanakya.valluri@gmail.com>
2019-11-07 12:56:51 -0800

    created contacts components and a left pane

---
+++ frontend/components/contacts/contacts.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Contacts extends Component {
+    render() {
+        return (
+            <div className="contacts-container">
+                Contacts
+            </div>
+        )
+    }
+}
