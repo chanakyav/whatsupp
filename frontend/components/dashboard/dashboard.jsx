3cf2cc0a1ef245183e42fa868032daaf30b13978
Chanakya <chanakya.valluri@gmail.com>
2019-11-07 10:04:53 -0800

    displaying dashboard

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Dashboard extends Component {
     render() {
         return (
-            <div>
-                dashboard
+            <div className="dashboard-container">
+                
             </div>
         )
     }
