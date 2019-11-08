10b5b7307a25364c4bb66faf43275bfc1266fa58
Chanakya <chanakya.valluri@gmail.com>
2019-11-08 06:58:03 -0800

    fixed profile back button

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -9,10 +9,9 @@ export default class Profile extends Component {
         return (
             <div className="profile-container">
                 <div className="sidenav-header">
-
-                    <span className="closebtn" onClick={() => this.closeNav()}>
-                        <i class="lni-arrow-left"></i>
-                    </span>
+                    <button id="closebtn" onClick={() => this.closeNav()}>
+                        <i className="fas fa-arrow-left"></i>
+                    </button>
                     <span>Profile</span>
                     
                 </div>
