f5e0a6add62ccd132ff068bb58f24a707fa22e23
Chanakya <chanakya.valluri@gmail.com>
2019-11-08 13:32:58 -0800

    refactored and styled contacts

--- frontend/components/contacts/contacts.jsx
+++ frontend/components/contacts/contacts.jsx
@@ -3,15 +3,18 @@ import React, { Component } from 'react'
 export default class Contacts extends Component {
     openNav() {
         document.querySelector("#mySidenav").style.width = "30%";
+        // document.querySelector(".left-pane").style.width = "0";
     }
     
     render() {
         return (
             <div className="contacts-container">
-                <span id="open-btn" onClick={() => this.openNav()}>
-                    <i class="far fa-user"></i>
-                </span>
-                <p>Contacts</p>
+                <div className="contacts-header">
+                    <span id="open-btn" onClick={() => this.openNav()}>
+                        <i class="far fa-user-circle"></i>
+                    </span>
+                    {/* <span>Contacts</span> */}
+                </div>
             </div>
         )
     }
