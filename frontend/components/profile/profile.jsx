7e9b5dec07a2dcfca69e46f3e6fbe1555283e91f
Chanakya <chanakya.valluri@gmail.com>
2019-11-09 21:11:39 -0800

    added hmtl to profile comp. to display user info

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -9,15 +9,45 @@ export default class Profile extends Component {
     render() {
         return (
             <div className="profile-container">
-                <div className="sidenav-header">
-                </div>
+                <div className="sidenav-header"></div>
                 <div className="sidenav-header">
                     <button id="closebtn" onClick={() => this.closeNav()}>
                         <i className="fas fa-arrow-left"></i>
                     </button>
                     <span>Profile</span>
                 </div>
+                <div className="profile-content">
+                    <div className="profile-picture">
+                        <div>
+                            <i className="fas fa-user-circle"></i>
+                        </div>
+                        <div>
+                            <i className="fas fa-camera"></i>
+                        </div>
+                        <p>Add profile photo</p>
+                    </div>
+                    <div className="profile-name-about">
+                        <ul>
+                            <li>Your Name</li>
+                            <li id="edit-name">
+                                <input type="text" placeholder="Full Name"/>
+                                <i className="fas fa-pen"></i>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                    </div>
+                    <div className="profile-name-about">
+                        <ul>
+                            <li>About</li>
+                            <li id="edit-name">
+                                <input type="text" placeholder="Available"/>
+                                <i className="fas fa-pen"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        )
+		);
     }
 }
