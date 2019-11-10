5a9a761082778f112cebe60e8f9b0a424f1a232c
Chanakya <chanakya.valluri@gmail.com>
2019-11-09 18:12:32 -0800

    changed profile position

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -10,13 +10,12 @@ export default class Profile extends Component {
         return (
             <div className="profile-container">
                 <div className="sidenav-header">
+                </div>
+                <div className="sidenav-header">
                     <button id="closebtn" onClick={() => this.closeNav()}>
                         <i className="fas fa-arrow-left"></i>
                     </button>
                     <span>Profile</span>
-                    
-                </div>
-                <div>
                 </div>
             </div>
         )
