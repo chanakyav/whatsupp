e2357706a246b1764f4b2a5d7420b976f6b34985
Chanakya <chanakya.valluri@gmail.com>
2019-11-09 17:47:32 -0800

    added more styling for contact items

--- frontend/components/contacts/contact_item.jsx
+++ frontend/components/contacts/contact_item.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 export const ContactItem = ({contact, deleteContact}) => {
+
     return (
-			<div className="contact-item">
-                
-				<div className="contact-user-icon">
-					<i className="far fa-user-circle"></i>
-				</div>
-				<ul>
-					<li>{`${contact.first_name} ${contact.last_name}`}</li>
-					<li>{contact.phone_number}</li>
-				</ul>
-			</div>
-		);
+        <div className="contact-item">
+            <div className="contact-user-icon">
+                <i className="far fa-user-circle user-icon"></i>
+            </div>
+            <div className="contact-info">
+                <ul>
+                    <li id="full-name">{`${contact.first_name} ${contact.last_name}`}</li>
+                    <li id="phone-number">{contact.phone_number}</li>
+                </ul>
+                <div className="contact-delete-btn">
+                    <button onClick={() => deleteContact(con)}>Delete</button>
+                </div>
+            </div>
+        </div>
+	);
 }
