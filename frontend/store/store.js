680f53e8de708a74f707f19acbac2530b6063c66
Chanakya <chanakya.valluri@gmail.com>
2019-11-11 10:44:10 -0800

    removed logger for heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/root_reducer";
 import thunk from "redux-thunk";
-import logger from "redux-thunk";
+// import logger from "redux-thunk";
 
-const middlewares = [thunk, logger];
+const middlewares = [thunk];
 
-// if (process.env.NODE_ENV !== "production") {
-//     // must use 'require' (import only allowed at top of file)
-//     const { logger } = require("redux-logger");
-//     middlewares.push(logger);
-// }
+if (process.env.NODE_ENV !== "production") {
+    // must use 'require' (import only allowed at top of file)
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) =>
     createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
