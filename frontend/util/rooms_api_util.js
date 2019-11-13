1901972b9ff3962b49f3c29ea1ed3dd8f5a4f7ee
Chanakya <chanakya.valluri@gmail.com>
2019-11-12 16:44:09 -0800

    refactor addroom api

--- frontend/util/rooms_api_util.js
+++ frontend/util/rooms_api_util.js
@@ -8,11 +8,6 @@ export function addRoom(roomData) {
     return $.ajax({
         method: "POST",
         url: `api/rooms`,
-        data: {
-            room: {
-                user1_id: roomData.room.user1_id,
-                user2_phone_number: roomData.room.user2_phone_number
-            }
-        }
+        data: {room: roomData}
     });
 }
