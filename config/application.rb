b2e6cbd3aef1ec2c0524f066e7007adf1f2f9c1b
Chanakya <chanakya.valluri@gmail.com>
2019-11-15 10:13:54 -0800

    heroku ready part 4

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Whatsupp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.assets.js_compressor = Uglifier.new(:harmony => true)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
