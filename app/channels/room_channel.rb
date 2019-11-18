b37ce115cd2cbfefcdba98eeb4da51a00a069d45
Chanakya <chanakya.valluri@gmail.com>
2019-11-18 09:53:42 -0800

    remove console log

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,9 +1,7 @@
 class RoomChannel < ApplicationCable::Channel
     def subscribed
-        p params
         room = Room.find(params[:room_id])
         stream_for room
-        # stream_from "room_channel"
     end
     
     def unsubscribed
