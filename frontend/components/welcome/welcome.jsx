b3b6d6bc49b9f8db5c17c4b43c21c3f6596bb92e
Chanakya <chanakya.valluri@gmail.com>
2020-01-12 21:24:46 -0800

    improve landing page layout

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -18,6 +18,15 @@ const Welcome = () => {
                 </div>
                 <div className="welcome-content-right">
                     <img src="https://www.zapptales.com/wp-content/uploads/2016/05/free-easter-mockup-generator-psd-1-3.png" alt="" srcset=""/>
+                    <div className="features3">
+                        <div>
+                            Explore Features
+                        </div>
+
+                        <div>
+                            Try Now!
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
