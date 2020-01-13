a74ca9f57cf6d072f43797743e1662ec0e7ba9f0
Chanakya <chanakya.valluri@gmail.com>
2020-01-12 21:07:49 -0800

    improve landing page

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Welcome = () => {
     return (
         <div className="login-box welcome-container">
-            <p >Welcome to WhatsUpp!</p>
+            {/* <p >Welcome to WhatsUpp!</p> */}
+            <div className="welcome-content">
+                <div className="welcome-content-left">
+                    <div className="features">
+                        Simple. Secure. 
+                    </div>
+                    <div className="features">
+                        Reliable messaging.
+                    </div>
+                    <div className="features2">  
+                        With WhatsApp, you'll get fast, simple, secure messaging available on phones and PC's all over the world.
+                    </div>
+                </div>
+                <div className="welcome-content-right">
+                    <img src="https://www.zapptales.com/wp-content/uploads/2016/05/free-easter-mockup-generator-psd-1-3.png" alt="" srcset=""/>
+                </div>
+            </div>
         </div>
     )
 }
